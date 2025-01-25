9aebb353a9f52907efc0a25becfc373e07d3b9da
qawith <qawith01@gmail.com>
2025-01-25 21:12:34 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,25 @@
 <?php
 
+namespace Tests\Feature;
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SurveyController;
+use Tests\TestCase;
 
 Route::get('/test', [SurveyController::class, 'show']);
 Route::post('/test', [SurveyController::class, 'submit']);
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_the_application_returns_a_successful_response()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+}

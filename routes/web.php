c3da292eaba6746e41d8227f9ab61cab9a07ebe9
qawith <qawith01@gmail.com>
2025-01-25 21:15:16 +0100

    update

--- routes/web.php
+++ routes/web.php
@@ -8,18 +8,3 @@ use Tests\TestCase;
 
 Route::get('/test', [SurveyController::class, 'show']);
 Route::post('/test', [SurveyController::class, 'submit']);
-
-class ExampleTest extends TestCase
-{
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_the_application_returns_a_successful_response()
-    {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
-    }
-}
